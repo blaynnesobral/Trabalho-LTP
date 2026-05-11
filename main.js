6e97ab99193a563518d7ee9a0dc8bf2406566121
blaynnesobral <blaynne.sobral@gmail.com>
2026-05-11 16:22:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const pronpt = require('prompt-sync') ();
+constLivro = require('./Livro')
+
+const livros = [];
+
+let opcao = -1;
+
+while (opcao !== 0){
+    console.log('1 - cadastrar livro');
+     console.log('2 - listar livro');
+      console.log('3 - alterar livro');
+       console.log('4 -  ');
+}
