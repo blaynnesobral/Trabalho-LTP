017f7e4894655a633a0b5ae1975bb7d1c24d58fb
blaynnesobral <blaynne.sobral@gmail.com>
2026-05-11 22:09:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
-class Livro {
-    constructor(titulo, autor, genero, anoPub, numPaginas) {
-        this.titulo = titulo;
-        this.autor = autor;
-        this.genero = genero;
-        this.anoPub = anoPub;
-        this.numPaginas = numPaginas;
+const prompt = require('prompt-sync')();
+const Livro = require('./Livro');
+
+const livros = [];
+
+let opcao;
+
+while (opcao !== 0) {
+
+    console.log("\n--- BIBLIOTECA ---");
+    console.log("1 - Cadastrar livro");
+    console.log("2 - Listar livros");
+    console.log("3 - Alterar livro");
+    console.log("4 - Estatísticas");
+    console.log("0 - Sair");
+
+    opcao = parseInt(prompt("Escolha: "));
+
+    if (isNaN(opcao)) {
+        console.log("Digite um número válido.");
+        continue;
     }
 
-    getInfo() {
-        return `${this.titulo} - ${this.autor} - ${this.genero} - ${this.anoPub} - ${this.numPaginas} páginas`;
+    switch(opcao) {
+
+        case 1:
+            console.log("Cadastrar");
+            break;
+
+        case 2:
+            console.log("Listar");
+            break;
+
+        case 3:
+            console.log("Alterar");
+            break;
+
+        case 4:
+            console.log("Estatísticas");
+            break;
+
+        case 0:
+            console.log("Saindo...");
+            break;
+
+        default:
+            console.log("Opção inválida.");
     }
 }
-
-module.exports = Livro;
