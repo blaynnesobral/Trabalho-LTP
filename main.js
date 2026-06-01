f04a6e54a3355f62f7c4aaeec86fdc5e8d40f4a5
asouzay <161075027+asouzay@users.noreply.github.com>
2026-06-01 11:28:03 -0400

    cases

--- main.js
+++ main.js
@@ -93,9 +93,38 @@ while (opcao !== 0) {
     console.log("Livro atualizado!");
 
     break;
-}
 
-        case 4:
+    
+        case 4: opcao = 4;
+             if (livros.length === 0) {
+        console.log("Nenhum livro cadastrado.");
+        break;
+    }
+
+    let totalPaginas = 0;
+    let generos = {};
+
+    livros.forEach(livro => {
+
+        totalPaginas += livro.numPaginas;
+
+        if (generos[livro.genero]) {
+            generos[livro.genero]++;
+        } else {
+            generos[livro.genero] = 1;
+        }
+
+    });
+
+    console.log("Média de páginas:",
+        totalPaginas / livros.length);
+
+    console.log("Livros por gênero:");
+
+    for (let genero in generos) {
+        console.log(`${genero}: ${generos[genero]}`);
+    }
+
             console.log("Estatísticas");
             break;
 
@@ -105,5 +134,7 @@ while (opcao !== 0) {
 
         default:
             console.log("Opção inválida.");
+
+            break;
     }
 }
